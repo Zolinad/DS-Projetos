99bcbb28eab365b1e80b59abbc0333594a5b980e
Danilo A. Figueiredo <daniloz.fig@gmail.com>
2025-12-29 15:03:53 -0300

    Update app_geo.py

--- projeto-2-geomarketing/app_geo.py
+++ projeto-2-geomarketing/app_geo.py
@@ -4,7 +4,7 @@ import numpy as np
 import plotly.express as px
 
 # Configuração da Página
-st.set_page_config(layout='wide', page_title="Geomarketing: Belém & Ananindeua")
+st.set_page_config(layout='wide', page_title="Inteligência de Varejo: Geomarketing Dashboard")
 
 # FUNÇÃO GERADORA DE DADOS (COM CORREÇÃO GEOGRÁFICA)
 @st.cache_data
